64bcd34391fbf1958b607fd0ec7135957f7534ca
baizeCrystal <1099703847@qq.com>
2026-06-07 16:18:38 +0800

    fix: clean remaining phantom params

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,12 @@
 from .kinematic_chain import KinematicChainReasoner
 from .phase_contrast_model import PhaseContrastActionErrorModel
-from .part_slot import PhaseAwarePartPrototypeAggregator, PhaseAwarePartSlotAggregator
+from .part_slot import PhaseAwarePartPrototypeAggregator
 from .skeleton_branch import PhaseAwareSkeletonAggregator, SkeletonKinematicEncoder
 from .soft_phase import SoftPhaseAssignment
 
 __all__ = [
     "KinematicChainReasoner",
     "PhaseAwarePartPrototypeAggregator",
-    "PhaseAwarePartSlotAggregator",
     "PhaseAwareSkeletonAggregator",
     "PhaseContrastActionErrorModel",
     "SkeletonKinematicEncoder",
